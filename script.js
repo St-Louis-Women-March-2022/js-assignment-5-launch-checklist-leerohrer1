c75c8d698045fbd2ec6724c2f0da3de4478497d3
leerohrer1 <leerohrer1@gmail.com>
2022-06-18 17:28:53 -0500

    Updated myFetch, addDestinationInfo functions and how they get called in script.js.

--- script.js
+++ script.js
@@ -27,5 +27,6 @@ window.addEventListener('load', function () {
     .then(function () {
       console.log(listedPlanets);
       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+      addDestinationInfo(pickPlanet(listedPlanets));
     });
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -10,18 +10,17 @@ function addDestinationInfo(
   moons,
   imageUrl
 ) {
-  // Here is the HTML formatting for our mission target div.
-  /*
+  document.getElementById('missionTarget').innerHTML = `
                 <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
+                    <li>Name: ${name}</li>
+                    <li>Diameter: ${diameter}</li>
                     <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Distance from Earth: ${distance}</li>
+                    <li>Number of Moons: ${moons}</li>
                 </ol>
-                <img src="">
-   */
+                <img src="${imageUrl}">
+   `;
 }
 
 function validateInput(testInput) {
@@ -143,7 +142,11 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 async function myFetch() {
   let planetsReturned;
 
-  planetsReturned = await fetch().then(function (response) {});
+  planetsReturned = await fetch(
+   'https://handlers.education.launchcode.org/static/planets.json'
+  ).then(function (response) {
+    return response.json;
+  });
 
   return planetsReturned;
 }
